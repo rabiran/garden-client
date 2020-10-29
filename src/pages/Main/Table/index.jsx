4f4fc2e648324b5db7d6994f156bc97759cb049a
grisha <codpure@gmail.com>
2020-10-29 11:36:54 +0200

    refactor: moved state of table to parent

--- src/pages/Main/Table/index.jsx
+++ src/pages/Main/Table/index.jsx
@@ -1,51 +1,25 @@
 import React from 'react';
 import './styles.css';
-import { getImmigrantsApi } from 'api/api'
 import Status from 'components/status';
-
 import MaterialTable from 'material-table'
 import Cell from './Cell';
 import hebrewLocalization from 'config/tableHebrew';
-
 import DeleteDialog from 'components/Confirm';
-
 import tableIcons from 'config/tableIcons';
-
 import DeleteIcon from '@material-ui/icons/Delete';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import { useSnackbar } from 'notistack';
-import useLoading from 'utils/LoadingProvider/useLoading';
 
 
-export default () => {
-    const loadingProvider = useLoading();
-    const { enqueueSnackbar } = useSnackbar();
+export default ({data, deleteFromTable}) => {
     const [openDelete, setOpenDelete] = React.useState(false);
-    const [tableData, setTableData] = React.useState();
-
-    React.useEffect(()=> {
-        async function fetchData() {
-            loadingProvider.showLoading(true);
-            const data = await getImmigrantsApi();
-            enqueueSnackbar('מידע התקבל', {variant: 'success', autoHideDuration: 2000});
-            loadingProvider.showLoading(false);
-            setTableData(data);
-        }
-        fetchData();
-    // eslint-disable-next-line 
-    }, [])
-
+    
     const handleOpenDelete = (data) => {
         setOpenDelete(data);
     }
 
     const handleConfirmDelete = () => {
-        console.log(openDelete)
-        const ids = openDelete.map(obj => obj.id);
-        const newTableData = tableData.filter((item) => !ids.includes(item.id));
-        setTableData(newTableData);
+        deleteFromTable(openDelete);
         setOpenDelete(false);
-        enqueueSnackbar('נמחק', {variant: 'success', autoHideDuration: 2000})
     }
 
     return (
@@ -54,6 +28,7 @@ export default () => {
                 localization={hebrewLocalization}
                 icons={tableIcons}
                 options={{
+                    // filtering: true,
                     selection: true,
                     selectionProps: rowData => ({
                         disabled: rowData.status.progress !== 'completed'
@@ -84,14 +59,14 @@ export default () => {
                                 return rowData.status.progress === 'completed';
                             }
                             return false;
-                         }
+                         },
                     },
-                    { title: 'שם מלא', field: 'fullName' },
-                    { title: 'תעודת זהות', field: 'identifier' },
-                    { title: 'תאריך יצירה', field: 'startDate' },
-                    { title: 'תאריך סיום', field: 'endDate' },
+                    { title: 'שם מלא', field: 'fullName', filtering: false },
+                    { title: 'תעודת זהות', field: 'identifier', filtering: false },
+                    { title: 'תאריך יצירה', field: 'startDate',filtering: false },
+                    { title: 'תאריך סיום', field: 'endDate', filtering: false },
                 ]}
-                data = {tableData}
+                data = {data}
                 title="מיגרנות"
                 detailPanel={ [{
                     icon: () => <ArrowBackIosIcon fontSize="small" />,
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,25 +1,46 @@
-import  React from 'react';
+import React from 'react';
 import './styles.css';
 import Table from './Table';
-
 import Dialog from './Dialog'
 import Grid from '@material-ui/core/Grid'
-// const useStyles = makeStyles(styles);
-
+import { getImmigrantsApi } from 'api/api';
+import { useSnackbar } from 'notistack';
+import useLoading from 'utils/LoadingProvider/useLoading';
 
 export default () => {
-    return (
-        <> 
-        <div className='GridContainer' >
-            <div className='tableContainer'>
-                <Table />
-            </div>
-            <div className='dialogContainer'>
-                <Dialog />
-            </div>
+    const loadingProvider = useLoading();
+    const { enqueueSnackbar } = useSnackbar();
+    const [tableData, setTableData] = React.useState();
 
-            
-        </div>
+    React.useEffect(()=> {
+        async function fetchData() {
+            loadingProvider.showLoading(true);
+            const data = await getImmigrantsApi();
+            enqueueSnackbar('מידע התקבל', {variant: 'success', autoHideDuration: 2000});
+            loadingProvider.showLoading(false);
+            setTableData(data);
+        }
+        fetchData();
+    // eslint-disable-next-line 
+    }, [])
+
+    const deleteFromTable = (data) => {
+        const ids = data.map(obj => obj.id);
+        const newTableData = tableData.filter((item) => !ids.includes(item.id));
+        setTableData(newTableData);
+        enqueueSnackbar('נמחק', {variant: 'success', autoHideDuration: 2000})
+    }
+
+    return (
+        <>
+            <div className='GridContainer' >
+                <div className='tableContainer'>
+                    <Table data={tableData} deleteFromTable={deleteFromTable}/>
+                </div>
+                <div className='dialogContainer'>
+                    <Dialog />
+                </div>
+            </div>
         </>
     )
 }
