d7a126f3f1d70780a61ca97503d9c0f8e13966fb
hagai245 <chagaimillot@gmail.com>
2020-11-10 15:15:18 +0200

    start chart

--- src/pages/Admin/index.jsx
+++ src/pages/Admin/index.jsx
@@ -1,10 +1,12 @@
 import  React from 'react';
+import Chart from 'components/Chart'
 // import styles from './styles';
 
 export default () => {
     return (
         <>
-            <p>hello admin</p>
+        hey
+            <Chart/>
         </>
     )
 }
