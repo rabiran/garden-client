027954035c37baaa6919a0c0348fe277449ac615
grisha <codpure@gmail.com>
2021-04-11 15:22:20 +0300

    fix: optimization and warnings removal and pagination fix

--- src/components/SubStep/index.jsx
+++ src/components/SubStep/index.jsx
@@ -2,7 +2,6 @@ import  React from 'react';
 import './styles.css';
 import Grid from '@material-ui/core/Grid';
 import DoneIcon from '@material-ui/icons/Done';
-import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 import BuildIcon from '@material-ui/icons/Build';
 import PauseIcon from '@material-ui/icons/Pause';
 import FeedbackIcon from '@material-ui/icons/Feedback';
